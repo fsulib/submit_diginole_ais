34d5cdb5c4e89ca01ee1e2c5365b7fac2facc55e
Bryan J. Brown <bjbrown@fsu.edu>
2022-09-24 00:41:55 -0400

    Resource types implementation complete

--- src/DiginoleSubmissionService.php
+++ src/DiginoleSubmissionService.php
@@ -197,6 +197,51 @@ class DiginoleSubmissionService {
     return true;
   }
 
+  protected function getSubmissionType(WebformSubmission $submission) {
+    $submission_data = $submission->getData();
+    $submission_form = $submission->get('webform_id')->target_id;
+    if ($submission_form == 'research_repository_submission') {
+      $submission_type = $submission_data['submission_type'];
+    }
+    else {
+      $submission_type = $submission_form;
+    }
+    return $submission_type;
+  }
+
+  protected function getResourceTypes(WebformSubmission $submission) {
+    $resourceTypeCrosswalk = [
+      "university_records_submission" => array('mods' => 'mixed material', 'rda' => 'unspecified', 'coar' => array('id' => 'c_1843', 'label' => 'other')),
+      "honors_thesis_submission" => array('mods' => 'text', 'rda' => 'text', 'coar' => array('id' => 'c_7a1f', 'label' => 'bachelor thesis')),
+      "3d_object" => array('mods' => 'three dimensional object', 'rda' => 'three-dimensional form', 'coar' => array('id' => 'c_1843', 'label' => 'other')),
+      "audio" => array('mods' => 'sound recording', 'rda' => 'sounds', 'coar' => array('id' => 'c_18cc', 'label' => 'sound')),
+      "book" => array('mods' => 'text', 'rda' => 'text', 'coar' => array('id' => 'c_2f33', 'label' => 'book')),
+      "book_chapter" => array('mods' => 'text', 'rda' => 'text', 'coar' => array('id' => 'c_3248', 'label' => 'book part')),
+      "capstone_project" => array('mods' => 'text', 'rda' => 'text', 'coar' => array('id' => 'c_46ec', 'label' => 'thesis')),
+      "conference" => array('mods' => 'text', 'rda' => 'text', 'coar' => array('id' => 'c_c94f', 'label' => 'conference output')),
+      "conference_paper" => array('mods' => 'text', 'rda' => 'text', 'coar' => array('id' => 'c_5794', 'label' => 'conference paper')),
+      "conference_poster" => array('mods' => 'text', 'rda' => 'text', 'coar' => array('id' => 'c_6670', 'label' => 'conference poster')),
+      "conference_presentation" => array('mods' => 'text', 'rda' => 'text', 'coar' => array('id' => 'R60J-J5BD', 'label' => 'conference presentation')),
+      "data_set" => array('mods' => 'mixed material', 'rda' => 'computer dataset', 'coar' => array('id' => 'c_ddb1', 'label' => 'dataset')),
+      "doctoral_nursing_program_capstone_project" => array('mods' => 'text', 'rda' => 'text', 'coar' => array('id' => 'c_46ec', 'label' => 'thesis')),
+      "editorial" => array('mods' => 'text', 'rda' => 'text', 'coar' => array('id' => 'c_b239', 'label' => 'editorial')),
+      "journal_article" => array('mods' => 'text', 'rda' => 'text', 'coar' => array('id' => 'c_6501', 'label' => 'journal article')),
+      "minimal" => array('mods' => 'text', 'rda' => 'unspecified', 'coar' => array('id' => 'c_1843', 'label' => 'other')),
+      "other" => array('mods' => 'text', 'rda' => 'unspecified', 'coar' => array('id' => 'c_1843', 'label' => 'other')),
+      "report" => array('mods' => 'text', 'rda' => 'text', 'coar' => array('id' => 'c_93fc', 'label' => 'report')),
+      "policy" => array('mods' => 'text', 'rda' => 'text', 'coar' => array('id' => 'c_186u', 'label' => 'policy report')),
+      "research" => array('mods' => 'text', 'rda' => 'text', 'coar' => array('id' => 'c_18ws', 'label' => 'research report')),
+      "technical" => array('mods' => 'text', 'rda' => 'text', 'coar' => array('id' => 'c_18gh', 'label' => 'technical report')),
+      "review" => array('mods' => 'text', 'rda' => 'text', 'coar' => array('id' => 'c_efa0', 'label' => 'review')),
+      "book_review" => array('mods' => 'text', 'rda' => 'text', 'coar' => array('id' => 'c_ba08', 'label' => 'book review')),
+      "video" => array('mods' => 'moving image', 'rda' => 'two-dimensional moving image', 'coar' => array('id' => 'c_12ce', 'label' => 'video')),
+      "working_paper" => array('mods' => 'text', 'rda' => 'text', 'coar' => array('id' => 'c_8042', 'label' => 'working paper')),
+    ];
+
+    $submission_type = $this->getSubmissionType($submission);
+    return $resourceTypeCrosswalk[$submission_type];
+  }
+
   protected function getCommonData(WebformSubmission $submission) {
     $template_data = [];
     $submission_data = $submission->getData();
@@ -220,6 +265,12 @@ class DiginoleSubmissionService {
     $template_data['location_purl'] = $submission_data['diginole_purl'];
     $template_data['identifier_iid'] = $this->getIID($submission);
     $template_data['note_grantNumber'] = $submission_data['grant_number'];
+
+    $resource_data = $this->getResourceTypes($submission);
+    $template_data['mods_resource'] = $resource_data['mods'];
+    $template_data['rda_resource'] = $resource_data['rda'];
+    $template_data['coar_resource_id'] = $resource_data['coar']['id'];
+    $template_data['coar_resource_label'] = $resource_data['coar']['label'];
 
     return $template_data;
   }
