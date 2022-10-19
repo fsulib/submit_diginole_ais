f6ba5eddbc48b5ddb89f2fe035700de31d02804e
Bryan J. Brown <bjbrown@fsu.edu>
2022-10-19 15:51:30 -0400

    Fixes embargo date calculation

--- src/SubmitDiginoleManifestService.php
+++ src/SubmitDiginoleManifestService.php
@@ -51,7 +51,8 @@ class SubmitDiginoleManifestService {
         $period_array = explode('_', $selected_period);
         $months = $period_array[0];
         $date_modifier = '+' . $months . ' month';
-        $date = new DateTime($submission_data['date_of_submission']);
+        $embargo_base_date = (!empty($submission_data['date_of_publication']) ? $submission_data['date_of_publication'] : $submission_data['date_of_submission']);
+        $date = new DateTime($embargo_base_date);
         $date->modify($date_modifier);
         $scholar_expiry = $date->format('Y-m-d');
       }
