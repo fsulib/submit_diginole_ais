8fb474e0605106ad8d55b5c04b4846214b832c32
Bryan J. Brown <bjbrown@fsu.edu>
2023-07-06 16:33:11 -0400

    Adds reporting to submission processing

--- src/Commands/ApprovedSubmissionCommands.php
+++ src/Commands/ApprovedSubmissionCommands.php
@@ -121,6 +121,12 @@ class ApprovedSubmissionCommands extends DrushCommands {
    * @usage submit_diginole_ais:process_submissions honors_thesis_submission --status=approved
    */
   public function processSubmissions($webform, $options = ['status' => 'approved']) {
+    $run_stats = array();
+    $run_stats['start'] = time();
+    $run_stats['processed'] = array();
+    $run_stats['succeeded'] = array();
+    $run_stats['failed'] = array();
+    $run_stats['packaged'] = array();
     $webformChoices = ["honors_thesis_submission","research_repository_submission","university_records_submission"];
 
     if (!in_array($webform, $webformChoices)) {
@@ -132,8 +138,12 @@ class ApprovedSubmissionCommands extends DrushCommands {
       if ($options['status']) {
         $status = $options['status'];
       }
+      $run_stats['webform'] = $webform;
+      $run_stats['status'] = $status;
       $sids = $this->diginoleSubmissionService->getSidsByFormAndStatus($webform, $status);
-      if (count($sids) > 0) {
+      $count = count($sids);
+      $run_stats['total'] = $count;
+      if ($count > 0) {
         $this->messenger->addMessage('Begin processing '. count($sids) . ' submissions.');
       }
       else {
@@ -142,57 +152,61 @@ class ApprovedSubmissionCommands extends DrushCommands {
       foreach ($sids as $sid) {
         $submission = $this->entityTypeManager->getStorage('webform_submission')->load($sid);
         $iid = $this->diginoleSubmissionService->getIID($submission);
+        $run_stats['processed'][] = "{$sid} / {$iid}";
         $this->messenger->addMessage("Processing {$iid}");
 
         $env = getenv('ENVIRONMENT');
-	switch ($env) {
+	      switch ($env) {
           case 'prod':
-	    $ais_env = 'prod';
-	    $base_url = "https://diginole.lib.fsu.edu";
+	          $ais_env = 'prod';
+	          $base_url = "https://diginole.lib.fsu.edu";
             $this->messenger->addMessage("Checking AIS-prod status of {$iid}.zip...");
             break;
           default:
-	    $ais_env = 'test';
-	    $base_url = "https://test.diginole.lib.fsu.edu";
+	          $ais_env = 'test';
+	          $base_url = "https://test.diginole.lib.fsu.edu";
             $this->messenger->addMessage("Checking AIS-test status of {$iid}.zip...");
             break;
         }
-	$ais_api_path = "/diginole/webservices/ais/package/status/";
-	$url = $base_url . $ais_api_path . $iid . ".zip";
+	      $ais_api_path = "/diginole/webservices/ais/package/status/";
+	      $url = $base_url . $ais_api_path . $iid . ".zip";
 
-	if (!$url) {
+	      if (!$url) {
           $ais_package_status = 'false';
         }
-	else {
+	      else {
           $ais_package_status = file_get_contents($url);
         }
 
-	$timestamp = date('Y-m-d', time());
+	      $timestamp = date('Y-m-d', time());
         if ($ais_package_status != 'false' && $status != 'rerun') {
-	  $response = json_decode($ais_package_status, TRUE);
-	  if ($response['status'] == 'Success') {
-	    $submission_log_message = "Submission {$iid} has been ingested by AIS-{$ais_env} to create {$response['message']}."; 
+	        $response = json_decode($ais_package_status, TRUE);
+	        if ($response['status'] == 'Success') {
+            $run_stats['succeeded'][] = "{$sid} / {$iid}";
+	          $submission_log_message = "Submission {$iid} has been ingested by AIS-{$ais_env} to create {$response['message']}."; 
             $this->messenger->addMessage($submission_log_message);
             $submission->setElementData('submission_status', 'ingested');
             $purlchecker_response = json_decode(file_get_contents($base_url . '/diginole/webservices/purlchecker/' . $iid), TRUE);
             $submission->setElementData('diginole_purl', $purlchecker_response['message']);
             $submission_log_message = "{$timestamp}: {$submission_log_message}";
           }
-	  else {
-	    $submission_log_message = "Submission {$iid} has been ingested by AIS-{$ais_env} but encountered the following error: '{$response['message']}'."; 
+	        else {
+            $run_stats['failed'][] = "{$sid} / {$iid}";
+	          $submission_log_message = "Submission {$iid} has been ingested by AIS-{$ais_env} but encountered the following error: '{$response['message']}'."; 
             $this->messenger->addMessage($submission_log_message);
             $submission->setElementData('submission_status', 'error');
             $submission_log_message = "{$timestamp}: {$submission_log_message}";
-	  }
+	        }
           $submission_log = $submission->getElementData('submission_log');
           $submission_log[] = $submission_log_message;
           $submission->setElementData('submission_log', $submission_log);
           $submission->resave();
-	  $submission = WebformSubmissionForm::submitWebformSubmission($submission);
+	        $submission = WebformSubmissionForm::submitWebformSubmission($submission);
           $this->messenger->addMessage("Skipping processing of {$iid}.");
         }
         else {
-	  $submission_log_message = "Submission {$iid} has not been ingested by AIS-{$ais_env}, creating ingest package.";
+          $run_stats['packaged'][] = "{$sid} / {$iid}";
+	        $submission_log_message = "Submission {$iid} has not been ingested by AIS-{$ais_env}, creating ingest package.";
           $this->messenger->addMessage($submission_log_message);
           $submission_log = $submission->getElementData('submission_log');
           $submission_log[] = "{$timestamp}: {$submission_log_message}";
@@ -260,6 +274,64 @@ class ApprovedSubmissionCommands extends DrushCommands {
         }
       }
     }
+    $run_stats['end'] = time();
+    $run_stats['length'] = intdiv($run_stats['end'] - $run_stats['start'], 60);
+    $run_stats_msg = "submit_diginole_ais log:<br>";
+    $run_stats_msg .= "Run Date: " . date("Y-m-d", $run_stats['start']) . "<br>"; 
+    $run_stats_msg .= "Run Duration: " . $run_stats['length'] . " minutes (" . $run_stats['start'] . " - " . $run_stats['end'] . ")<br>"; 
+    $run_stats_msg .= "Target Webform: {$run_stats['webform']}<br>";
+    $run_stats_msg .= "Target Status: {$run_stats['status']}<br>";
+    $run_stats_msg .= "<br>";
+
+    
+    if (!empty($run_stats['processed'])) {
+      $run_stats_msg .= "Processed submissions:<br>";
+      foreach ($run_stats['processed'] as $processed) {
+        $run_stats_msg .= "- {$processed}<br>";
+      }
+    }
+    else {
+      $run_stats_msg .= "No processed submissions.<br>";
+    }
+    $run_stats_msg .= "<br>";
+
+
+    if (!empty($run_stats['succeeded'])) {
+      $run_stats_msg .= "Successful prior ingests:<br>";
+      foreach ($run_stats['succeeded'] as $succeeded) {
+        $run_stats_msg .= "- {$succeeded}<br>";
+      }
+    }
+    else {
+      $run_stats_msg .= "No successful prior ingests.<br>";
+    }
+    $run_stats_msg .= "<br>";
+
+    if (!empty($run_stats['failed'])) {
+      $run_stats_msg .= "Failed prior ingests:<br>";
+      foreach ($run_stats['failed'] as $failed) {
+        $run_stats_msg .= "- {$failed}<br>";
+      }
+    }
+    else {
+      $run_stats_msg .= "No failed prior ingests.<br>";
+    }
+    $run_stats_msg .= "<br>";
+
+    if (!empty($run_stats['packaged'])) {
+      $run_stats_msg .= "Packaged submissions:<br>";
+      foreach ($run_stats['packaged'] as $packaged) {
+        $run_stats_msg .= "- {$packaged}<br>";
+      }
+    }
+    else {
+      $run_stats_msg .= "No packaged submissions.<br>";
+    }
+    $run_stats_msg .= "<br>";
+
+    $this->messenger->addMessage($run_stats_msg);
+    \Drupal::logger('submit_diginole_ais_processing_log')->info($run_stats_msg);
+
   }
 
 }
